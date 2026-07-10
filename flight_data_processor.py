aa069a4efc6d069ac8d2b7fe0066aaa39672a215
praveensundar2501 <praveen25kumar94@gmail.com>
2026-07-10 11:21:10 +0530

    Update flight_data_processor.py
    passing value to code

--- flight_data_processor.py
+++ flight_data_processor.py
@@ -60,46 +60,46 @@ class FlightDataProcessor:
         raise ValueError(f"Flight {flight_number} not found.")
 
 
+# Testing FlightDataProcessor class
+
+# Initialize test data with a default flight record
+test_data = [
+    {"flight_number": "AZ001", "departure_time": "2025-02-19 15:30",
+     "arrival_time": "2025-02-20 03:45", "status": "ON_TIME"}
+]
+
+# Create an instance of FlightDataProcessor with test data
+flight = FlightDataProcessor(test_data)
+
+# New flight record to be added
+new_flight_data = {
+    "flight_number": "AZ002", "departure_time": "2025-02-21 11:00",
+    "arrival_time": "2025-02-21 16:00", "status": "DELAYED"
+}
+
+# Adding a new flight record
+print("#" * 5 + " Adding a New Flight " + "#" * 15)
+print(flight.add_flight(new_flight_data))
+
+# Removing a flight record
+print("#" * 5 + " Removing a Flight " + "#" * 15) 
+flight_number = 'AZ001'  # Set the flight number to remove
+print(flight.remove_flight(flight_number))
+
+# Finding the longest flight based on duration
+print("#" * 5 + " Finding the Longest Flight " + "#" * 15)
+print(flight.get_longest_flight())
+
+# Updating the status of a flight
+print("#" * 5 + " Updating Flight Status " + "#" * 15)
+
+# Define the flight number and the new status to be updated
+flight_number = 'AZ001'
 
 
 
 
 
-# # Testing FlightDataProcessor class
-
-# # Initialize test data with a default flight record
-# test_data = [
-#     {"flight_number": "AZ001", "departure_time": "2025-02-19 15:30",
-#      "arrival_time": "2025-02-20 03:45", "status": "ON_TIME"}
-# ]
-
-# # Create an instance of FlightDataProcessor with test data
-# flight = FlightDataProcessor(test_data)
-
-# # New flight record to be added
-# new_flight_data = {
-#     "flight_number": "AZ002", "departure_time": "2025-02-21 11:00",
-#     "arrival_time": "2025-02-21 16:00", "status": "DELAYED"
-# }
-
-# # Adding a new flight record
-# print("#" * 5 + " Adding a New Flight " + "#" * 15)
-# print(flight.add_flight(new_flight_data))
-
-# # Removing a flight record
-# print("#" * 5 + " Removing a Flight " + "#" * 15) 
-# flight_number = 'AZ001'  # Set the flight number to remove
-# print(flight.remove_flight(flight_number))
-
-# # Finding the longest flight based on duration
-# print("#" * 5 + " Finding the Longest Flight " + "#" * 15)
-# print(flight.get_longest_flight())
-
-# # Updating the status of a flight
-# print("#" * 5 + " Updating Flight Status " + "#" * 15)
-
-# # Define the flight number and the new status to be updated
-# flight_number = 'AZ001'
 # status = 'CANCELLED'
 
 # print(flight.update_flight_status(flight_number, status))
